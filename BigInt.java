3c6bf69501273b4ab805eb21f54231c31004097b
enigmadan <enigmadan@gmail.com>
2013-02-13 16:02:35 -0800

    Create BigInt.java

---
+++ BigInt.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+public class BigIntTest {
+	public static void main(String [] args){
+		String input = args[0];
+		String output = args[1];
+		String operator;
+		ObjectInputStream ioStream = null;
+		ObjectOutputStream ooStream = null;
+		if (!(args.length == 2)){
+			System.err.println("Usage: java BigIntTest infile outfile");
+			System.exit(1);
+		}
+		try{
+			ioStream = new ObjectInputStream( new FileInputStream(input));
+			Scanner s = new Scanner(ioStream);
+			BigInt bi1 = new BigInt(s.next());
+			operator = s.next();
+			BigInt bi2 = new BigInt(s.next());
+			
+			
+			
+		}
+		catch(IOException e){
+			System.err.println("The file '"+input+"' could not be opened for reading.");
+			System.exit(1);
+		}
+
+	}
+
+}
