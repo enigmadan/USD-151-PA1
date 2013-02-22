72636340c000245015f5c39bdef5b26862712d3e
enigmadan <enigmadan@gmail.com>
2013-02-21 17:45:52 -0800

    Update BigIntTest.java

--- BigIntTest.java
+++ BigIntTest.java
@@ -1,31 +1,35 @@
 import java.io.*;
 import java.util.Scanner;
-public class BigIntTest {
-  public static void main(String [] args){
-    String input = args[0];
-    String output = args[1];
-    String operator;
-    ObjectInputStream ioStream = null;
-    ObjectOutputStream ooStream = null;
-    if (!(args.length == 2)){
-      System.err.println("Usage: java BigIntTest infile outfile");
-      System.exit(1);
-    }
-    try{
-      ioStream = new ObjectInputStream( new FileInputStream(input));
-      Scanner s = new Scanner(ioStream);
-      BigInt bi1 = new BigInt(s.next());
-      operator = s.next();
-      BigInt bi2 = new BigInt(s.next());
+public class testr {
+    public static void main(String [] args){
+        if (!(args.length == 2)){
+            System.err.println("Usage: java BigIntTest infile outfile");
+            System.exit(1);
+        }
+        try{
+            FileReader src = new FileReader(args[0]);
+            PrintWriter pw = new PrintWriter(args[1]);
+            Character operator;
+            //idea string tokenizer
+           
 
+        Scanner s = new Scanner(src);
+       
+            while (s.hasNext()){
+               
+                BigInt bi1 = new BigInt(s.next());
+                operator = (s.next()).charAt(0);
+                BigInt bi2 = new BigInt(s.next());
+                pw.println(BigInt.printProb(bi1, operator, bi2));
+            }
 
-  
-    }
-    catch(IOException e){
-      System.err.println("The file '"+input+"' could not be opened for reading.");
-      System.exit(1);
-    }
+        }
+        catch(IOException e){
+            System.err.println("The file '"+args[0]+"' could not be opened for reading.");
+            System.exit(1);
+        }
 
-  }
+        System.out.println("Done!");
+    }
 
 }
