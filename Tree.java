6b4e3c63bbad5338104b3b8084c525002979c727
enigmadan <enigmadan@gmail.com>
2013-02-27 18:05:27 -0800

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,7 +1,8 @@
 /**
  * File: Tree.java
- * @author
- * @version
+ * @author Mingyu Chen
+ * @author Daniel Myers
+ * @version 2/27/13
  * Description: Class to display a Tree fractal.
  */
 
@@ -21,36 +22,29 @@ import javax.imageio.ImageIO;
  */
 public class Tree
 {
-    public static void main(String[] args)
+	public static void main(String[] args)
 	{
-		/*// Get user parameters.
-Scanner keyboardInput = new Scanner(System.in);
-System.out.println("Enter length of central trunk, "
-+ "as an integer number of pixels: ");
-final int size = keyboardInput.nextInt();
-System.out.println("Enter depth of recursion "
-+ "(0 represents just drawing the triangle): ");
-final int maxLevelsOfRecursion = keyboardInput.nextInt();
-System.out.println("Enter number of branches per branch: ");
-final int numBranches = keyboardInput.nextInt();
-System.out.println("Enter length of subbranch as a "
-+ "fraction of the length of its parent: ");
-final double lengthScaleFactor = keyboardInput.nextDouble();
-System.out.println("Enter the angle that a subbranch "
-+ "makes with its parent: ");
-final double branchAngle = keyboardInput.nextDouble();
-System.out.println("Print files (1) or not (0): ");
-final int printFiles = keyboardInput.nextInt();
-		 */
+		// Get user parameters.
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.println("Enter height of central trunk, "
+				+ "as an integer number of pixels: ");
+		final int size = keyboardInput.nextInt();
+		System.out.println("Enter depth of recursion "
+				+ "(0 represents just drawing the trunk): ");
+		final int maxLevelsOfRecursion = keyboardInput.nextInt();
+		System.out.println("Enter number of branches per branch: ");
+		final int numBranches = keyboardInput.nextInt();
+		System.out.println("Enter length of subbranch as a "
+				+ "fraction of the length of its parent: (eg .6)");
+		final double lengthScaleFactor = keyboardInput.nextDouble();
+		System.out.println("Enter the angle in degrees that a subbranch "
+				+ "makes with its parent: ");
+		final double branchAngle = keyboardInput.nextDouble();
+		System.out.println("Print files (1) or not (0): ");
+		final int printFiles = keyboardInput.nextInt();
+
 
 		// Get everything going.
-		// Get user parameters.
-		final int size = 300;
-		final int maxLevelsOfRecursion = 4;
-		final int numBranches = 4;
-		final double lengthScaleFactor = .6;
-		final double branchAngle = 7;
-		final int printFiles = 0;
 
 		EventQueue.invokeLater(new Runnable()
 		{
@@ -149,28 +143,29 @@ of the length of the branch it is coming off of.
 		// Draw the Tree Fractal.
 		drawFractalTree((Graphics2D) g);
 
-		// Print the image to a file, if asked.
-		if (printFiles)
-		{
-			BufferedImage bi = new BufferedImage(2 * size, 2 * size,
-					BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g2 = bi.createGraphics();
-			g2.setColor(Color.white);
-			g2.fillRect(0, 0, 2 * size, 2 * size);
-			g2.setColor(Color.black);
-			drawFractalTree(g2);
-			try
-			{
-				ImageIO.write(bi, "JPEG", new File("Fractal Tree " +
-						currentLevelOfRecursion + ".jpg"));
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
+	     // Print the image to a file, if asked.
+	      if (printFiles)
+	      {
+	         BufferedImage bi = new BufferedImage(2 * size, 2 * size, 
+	                                          BufferedImage.TYPE_INT_ARGB);
+	         Graphics2D g2 = bi.createGraphics();
+	         g2.setColor(Color.white);
+	         g2.fillRect(0, 0, 2 * size, 2 * size);
+	         g2.setColor(Color.black);
+	         drawFractalTree(g2);
+	         try
+	         {
+	            ImageIO.write(bi, "JPEG", new File("KochSnowflake" + 
+	                                  currentLevelOfRecursion + ".jpg"));
+	         }
+	         catch (IOException e) 
+	         {
+	            e.printStackTrace();
+	         }
+	      }
 	}
 	public void drawFractalTree(Graphics2D g2){
+
 
 		drawLine(g2, currentLevelOfRecursion, p1, p2);
 	}
@@ -182,41 +177,43 @@ of the length of the branch it is coming off of.
 		double imRad = (branchAngle) * Math.PI / 180;
 		double angle = Math.atan(Math.abs((p2.getX() - p1.getX())/(p2.getY()-p1.getY())));
 		Point2D	pB;
-		g2.setColor(new Color(118-((currentLevelOfRecursion-n)*54/(currentLevelOfRecursion+1)),
-				64+((currentLevelOfRecursion-n)*40/(currentLevelOfRecursion+1)),
-				41-((currentLevelOfRecursion-n)*19/(currentLevelOfRecursion+1))));
+		g2.setColor(new Color(118-((currentLevelOfRecursion-n)*97/(maxLevelsOfRecursion)),
+				64+((currentLevelOfRecursion-n)*23/(maxLevelsOfRecursion)),
+				41-((currentLevelOfRecursion-n)*41/(maxLevelsOfRecursion))));
+		g2.setStroke(new BasicStroke(10 -((currentLevelOfRecursion-n)*9.75f/(currentLevelOfRecursion+1)),BasicStroke.CAP_ROUND,BasicStroke.JOIN_MITER));		
+		g2.draw(new Line2D.Double(p1, p2));
+
 		if (n != 0)
 		{
 			for(int i = 1;i <= numBranches;i++){
-				
+
 				Point2D pA = new Point2D.Double(p1.getX() + (i * (p2.getX() - p1.getX()) / (numBranches + 1)),
 						p1.getY() + (i * (p2.getY() - p1.getY()) / (numBranches + 1)));
 				double x;
 				double y;
-				
+
 				if((p1.getX()-p2.getX() > 0)){
-					
+
 					x = pA.getX() + Math.sin(angle - Math.PI + (i % 2 == 0 ? -1 : 1) * imRad) * hyp;
 				}else{
 					x = pA.getX() + Math.sin(angle + (i % 2 == 0 ? -1 : 1) * imRad) * hyp;
 
 				}
-				
+
 				if((p1.getY() - p2.getY() < 0)){
-					
+
 					y = pA.getY() - Math.cos(angle - Math.PI + (i % 2 == 0 ? -1 : 1) * imRad) * hyp;
 				}else{
 					y = pA.getY() - Math.cos(angle + (i % 2 == 0 ? -1 : 1) * imRad) * hyp;	
-					
+
 				}
-					pB = new Point2D.Double(x, y);
-					
+				pB = new Point2D.Double(x, y);
+
 				drawLine(g2, n-1, pA, pB);
 
 			}
 
 		}
-		g2.draw(new Line2D.Double(p1, p2));
 
 	}
 	// Private fields.
